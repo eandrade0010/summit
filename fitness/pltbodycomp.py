e1d6b6da28e47a3d91586ac7acc08e6f195a27e2
eandrade0010 <eandrade0010@gmail.com>
2018-09-30 19:20:41 -0700

    updating body comp

--- fitness/pltbodycomp.py
+++ fitness/pltbodycomp.py
@@ -3,6 +3,12 @@
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 from datetime import datetime
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--filter", help="You can choose to reduce noise of data", action='store_true')
+args = parser.parse_args()
+
 
 content = open('body-comp.csv', 'r')
 
@@ -21,18 +27,19 @@ for i, line in enumerate(content):
     goal.append(line[4])
 
 
-print(dates)
-print(weight)
-print(bf)
 
 fig, ax1 = plt.subplots()
-ax1.plot_date(dates, weight, '.k')
+ax1.plot_date(dates, weight, 'k', label="Weight")
 ax1.set_xlabel('Date')
 ax1.set_ylabel('Weight [lbs]')
 
 ax2 = ax1.twinx()
-ax2.plot_date(dates, bf, '*r')
+ax2.plot_date(dates, bf, 'r', label="BF")
 ax2.set_ylabel('BF [%]')
-plt.xticks(dates, rotation='vertical')
 
+
+fig.autofmt_xdate()
+fig.legend(loc="upper right")
+plt.title('Body Composition')
+# plt.show()
 plt.savefig('body_comp.jpg')
