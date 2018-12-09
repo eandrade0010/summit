94999551e0b9f16e648dae6f3b3b9d08f958396f
eandrade0010 <eandrade0010@gmail.com>
2018-12-09 12:42:08 -0800

    body comp

--- fitness/pltbodycomp.py
+++ fitness/pltbodycomp.py
@@ -27,7 +27,10 @@ for i, line in enumerate(content):
     bf.append(float(line[2]))
     goal.append(line[3])
 
-# AVERAGE
+
+# ==========================================================
+# AVERAGES
+# ==========================================================
 avg_weight = [0]*len(weight)
 avg_bf = [0]*len(bf)
 for i in range(len(weight)):
@@ -41,11 +44,15 @@ for i in range(len(weight)):
         avg_weight[i] = (weight[i-1]+weight[i]+weight[i+1])/3
         avg_bf[i] = (bf[i-1]+bf[i]+bf[i+1])/3
 
+
+# ==========================================================
+# CHANGES
+# ==========================================================
 change_weight = []
 week = []
 for i in range(0,len(weight)-7,7):
     print(i)
-    week.append(i)
+    week.append(int(i/7))
     change_weight.append(weight[i+7]-weight[i])
 
 weight = avg_weight
@@ -56,7 +63,7 @@ bf = avg_bf
 print(change_weight)
 
 # PLOTTING
-fig, (ax1,ax3) = plt.subplots(2,1)
+fig1, ax1 = plt.subplots()
 ax1.plot_date(dates, weight, 'k', label="Weight")
 ax1.set_xlabel('Date')
 ax1.set_ylabel('Weight [lbs]')
@@ -65,15 +72,22 @@ ax2 = ax1.twinx()
 ax2.plot_date(dates, bf, 'r', label="BF")
 ax2.set_ylabel('BF [%]')
 
-ax3.plot(week,change_weight, '--k', label="Change in Weight")
+# ax3.plot(week,change_weight, '--k', label="Change in Weight")
 
 # ax4 = ax3.twinx()
 # ax4.plot_date(dates, change_bf, '--r', label="Change in BF")
 
-fig.autofmt_xdate()
-fig.legend(loc="upper right")
+fig1.autofmt_xdate()
+fig1.legend(loc="upper right")
 
-fig.suptitle('Body Composition with Average Filter')
-if args.save: plt.savefig('body_comp.jpg')
+fig1.suptitle('Body Composition with Average Filter')
+
+fig2, ax3 = plt.subplots()
+ax3.plot(week, change_weight, 'k', label="Change in Weight")
+# ax4 = ax3.twinx()
+# ax4.plot(week, change_bf, 'r', label="Change in BF")
+# ax4.
+
+if args.save: fig1.savefig('body_comp.jpg')
 plt.show()
 # print(avg_weight)
